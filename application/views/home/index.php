b019c285484e4d4f5d21f31855207e0d59e91adb
KingsDevs <sunnyckair@gmail.com>
2021-07-23 02:33:46 +0800

    working with newlayout

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -8,41 +8,22 @@
                         <a href="<?php echo base_url('addwish') ?>" class="btn btn-primary float-end">Add Wish</a>
                     </h6>
                 </div>
-
-                <div class="card-body">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Edit/Delete</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody>
-                            <!-- <?php foreach($wishlist as $wd) : ?>
-                            <tr>
-                                <td> <?=$wd->date; ?></td>
-                                <td> <?=$wd->title; ?></td>
-                                <td> <?=$wd->description; ?></td>
-                                <td><a href="<?php echo base_url('home/edit/'.$wd->id) ?>" class="btn btn-primary">Edit</a>
-                                    <a href="<?php echo base_url('home/delete/'.$wd->id) ?>" class="btn btn-danger">Delete</a>
-                                </td>
-                            </tr>
-                            <?php endforeach; ?> -->
-
-                            <div class="card mb-3">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
-                        </tbody>
-                    </table>
-                </div>
+                
+                <?php foreach($wishlist as $wd) : ?>
+                    <div class="card mb-3">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?=$wd->title; ?></h5>
+                            <p class="card-text"><?=$wd->description; ?></p>
+                            <p class="card-text"><small class="text-muted"><?=$wd->date; ?></small></p>
+                            <a href="<?php echo base_url('home/edit/'.$wd->id) ?>" class="btn btn-primary">Edit</a>
+                            <a href="<?php echo base_url('home/delete/'.$wd->id) ?>" class="btn btn-danger">Delete</a>
+                        </div>
+                    </div>
+                
+                  
+                <?php endforeach; ?>
+              
             </div>
         </div>
     </div>
